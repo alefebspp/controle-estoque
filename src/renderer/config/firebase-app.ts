fe042b1d623a602444f34660a4c0e525af01f966
Alefe Bispo <alefebsp@hotmail.com>
2024-01-17 18:25:19 -0300

    remove firebase config comments

--- src/renderer/config/firebase-app.ts
+++ src/renderer/config/firebase-app.ts
@@ -1,7 +1,5 @@
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: 'AIzaSyAfqbzDl4UlUSpDegwwPNqAHe3C1nOmMJ4',
   authDomain: 'controle-estoque-96d8a.firebaseapp.com',
@@ -11,8 +9,5 @@ const firebaseConfig = {
   appId: '1:681437577378:web:74ace5d1b38687b553d7fb',
   measurementId: 'G-GPZ18XHWYC',
 };
-// Initialize Firebase
 
 export const app = initializeApp(firebaseConfig);
-// Export firestore database
-// It will be imported into your react app whenever it is needed
